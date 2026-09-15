12ab729309c9a111b9bbf54a81b63286479c7f87
Nirbhay-tech <nirbhayruhela0001@gmail.com>
2026-09-15 21:34:15 +0530

    Learn Queue

---
+++ Java Collection Framework/LearnQueue.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class LearnQueue{
+    public static void main(String[] args){
+        Queue<Integer> q = new  LinkedList<>();
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+
+        System.out.println(q);
+
+        // System.out.println(q.poll());
+
+        // System.out.println(q);
+
+        // System.out.println(q.peek());
+
+        // System.out.println(q);
+
+        while(!q.isEmpty()){
+            System.out.println(q.poll());
+        }
+    }
+}
